5d63add7744541d5ad06e799b3a9bcacf937a977
MarquezLuis96 <englamc@gmail.com>
2020-07-09 01:39:48 -0400

    V.1.0.1.1

--- 03 - Second project - Drawing on canvas/01 - learning about window & document/js/codigo.js
+++ 03 - Second project - Drawing on canvas/01 - learning about window & document/js/codigo.js
@@ -1,1 +1,8 @@
-alert("It works!");
+//trying if the file was well linked
+    alert("It works!");
+// saving path
+    var path = window.location;
+//viewing path on the console    
+    console.log(path);
+//Wrinting the path on the document    
+document.write(`<p>The path of this file is:<br><strong>${path}</strong></p>`);
