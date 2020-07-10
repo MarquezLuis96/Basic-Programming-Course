8433878b85eabc7f548c423e0c06c530c1e30778
MarquezLuis96 <englamc@gmail.com>
2020-07-10 18:20:36 -0400

    V.1.0.1.3

--- 03 - Second project - Drawing on canvas/02 - Drawing Canvas - DOM/js/code.js
+++ 03 - Second project - Drawing on canvas/02 - Drawing Canvas - DOM/js/code.js
@@ -25,3 +25,17 @@ lienzo.moveTo(180, 10);
 lienzo.lineTo(290, 200);
 lienzo.stroke();
 lienzo.closePath();
+
+drawLine("orange", 100, 100, 300, 50);
+drawLine("pink", 10, 300, 220, 10);
+drawLine("white", 310, 10, 10, 220);
+drawLine("cyan", 220, 290, 59, 125);
+
+function drawLine(color, x1, y1, x2, y2) {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(x1, y1);
+    lienzo.lineTo(x2, y2);
+    lienzo.stroke();
+    lienzo.closePath();
+}
