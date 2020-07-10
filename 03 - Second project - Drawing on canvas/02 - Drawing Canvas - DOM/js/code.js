17cb22f4e23a0fa64993030fc492240c52de573d
MarquezLuis96 <englamc@gmail.com>
2020-07-10 17:52:35 -0400

    V.1.0.1.2

--- 03 - Second project - Drawing on canvas/02 - Drawing Canvas - DOM/js/code.js
+++ 03 - Second project - Drawing on canvas/02 - Drawing Canvas - DOM/js/code.js
@@ -1,5 +1,27 @@
+alert("Welcome to canvas");
+
 var d = document.getElementById("littledraw");
 var lienzo = d.getContext("2d");
 
+console.log(lienzo);
+
 lienzo.beginPath();
-lienzo.strokeStyle
+lienzo.strokeStyle = "red";
+lienzo.moveTo(100, 100);
+lienzo.lineTo(200, 200);
+lienzo.stroke();
+lienzo.closePath();
+
+lienzo.beginPath();
+lienzo.strokeStyle = "blue";
+lienzo.moveTo(50, 10);
+lienzo.lineTo(250, 2);
+lienzo.stroke();
+lienzo.closePath();
+
+lienzo.beginPath();
+lienzo.strokeStyle = "yellow";
+lienzo.moveTo(180, 10);
+lienzo.lineTo(290, 200);
+lienzo.stroke();
+lienzo.closePath();
