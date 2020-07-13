f41becee6b9139e156002ef90049329f5968e84b
MarquezLuis96 <englamc@gmail.com>
2020-07-13 10:01:18 -0400

    V.2.0.0.0

--- 03 - Second project - Drawing on canvas/05 - Detect keyboard events with JavaScript/js/code.js
+++ 03 - Second project - Drawing on canvas/05 - Detect keyboard events with JavaScript/js/code.js
@@ -9,19 +9,44 @@ console.log(keys);
 
 document.addEventListener("keyup", keyDraw);
 
+var d = document.getElementById("paper");
+var lienzo = d.getContext("2d");
+var x = 150;
+var y = 150;
+
+function drawLine(color, x1, y1, x2, y2) {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
+    lienzo.moveTo(x1, y1);;
+    lienzo.lineTo(x2, y2);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+drawLine("red", 149, 149, 151, 151);
+
 function keyDraw(evento) {
+    //Line color
+    var color = "red";
+    var move = 10;
+
     switch(evento.keyCode) {
         case keys.UP :
-                console.log("UP");
+                console.log("UP");        
+                drawLine(color, x , y , x, (y-=move));
             break;
         case keys.LEFT:
                 console.log("LEFT");
+                drawLine(color, x, y, (x-=move), y);
             break;
         case keys.RIGHT:
                 console.log("RIGHT");
+                drawLine(color, x, y, (x+=move), y);
             break;
         case keys.DOWN:
                 console.log("DOWN");
+                drawLine(color, x, y, x, (y+=move));
             break;
         default:
                 console.log("Another key");
