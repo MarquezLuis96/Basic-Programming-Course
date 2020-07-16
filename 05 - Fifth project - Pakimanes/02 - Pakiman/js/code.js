9f235089108737e0c3a0a32e3af4994664859c21
MarquezLuis96 <englamc@gmail.com>
2020-07-16 12:57:48 -0400

    V.4.0.0.2

--- 05 - Fifth project - Pakimanes/02 - Pakiman/js/code.js
+++ 05 - Fifth project - Pakimanes/02 - Pakiman/js/code.js
@@ -1,15 +1,29 @@
+var pakimages = [];
+pakimages["CowShu"] = "CowShu.png";
+pakimages["Poposho"] = "Poposho.png";
+pakimages["Baconrad"] = "Baconrad.png";
+pakimages["Wolfestein"] = "Wolfestein.png";
+
 class Pakiman {
     constructor(name, type, life, attack) {
+        this.image = new Image();
         this.name = name;
         this.type = type;
         this.life = life;
         this.attack = attack;
+
+        this.image.src = pakimages[this.name];
+    }
+
+    speak() {
+        window.alert(this.name);
     }
 }
 
 var cowShu = new Pakiman("CowShu", "Earth", 100, 30);
-var chikenPaThai = new Pakiman("chikenPaThai", "Air", 80, 50);
-var baconrad = new Pakiman("Baconrad", "Earth", 120, 40);
-var wolfestain = new Pakiman("Wolfestain", "fire", 70, 55);
 
-console.log(cowShu);
+var poposho = new Pakiman("Poposho", "Air", 80, 50);
+
+var baconrad = new Pakiman("Baconrad", "Earth", 120, 40);
+
+var wolfestein = new Pakiman("Wolfestein", "fire", 70, 55);
